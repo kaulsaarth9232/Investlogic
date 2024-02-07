8104c5485f8c4aa3b0761fdb21bbfe7c9cf76c43
kaul9232 <kaulsaarth@gmail.com>
2024-02-07 13:27:43 +0530

    added files

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors");
-const database = require("./repositories/connectDb");
-const router = require("./routes");
-
-// Creating an instance of express
-const app = express();
+const { urlencoded } = require('express');
+const express = require('express');
+const LoginRoute = require('./Routes/login')
+const SignupRoute = require('./Routes/signup')
+const Conn = require('./DB/db');
+const cors = require('cors');
+Conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+const app = express()
 app.use(express.json())
-app.use(morgan('dev')) // using morgan for logging details
-app.use(cors()); // cors to connect frontend and backend
-
-database; // Calling databse
-
-const PORT = 8000; // defining port
-
-app.use('/user', router); // Defining router 
-
-// Listening on port
-app.listen(PORT, ()=> {
-    console.log(`Listening on Port - ${PORT}`);
+app.use(cors())
+app.listen(5000, () => {
+    console.log('listening on port 5000')
 })
+
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    console.log('backend connected')
+})
+app.use('/login', LoginRoute)
+app.use('/signup', SignupRoute)
